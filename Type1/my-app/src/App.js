9930face0824c90dbd92c84229db9245407f3a60
mamillapallishilpa1997 <mamillapallishilpa1997@gmail.com>
2026-05-24 14:49:36 +0530

    Initial commit

---
+++ Type1/my-app/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return <><div class="overlay"></div><div class="card">
+
+    <div class="card-top"></div>
+
+    <div class="icon">
+      🎓
+    </div>
+
+    <div class="card-body">
+      <h2>Miracle Kids Pre School</h2>
+      <p>Welcome! Click below to enter our school website.</p>
+
+      <button class="btn" id="button_school_site">Enter Site</button>
+    </div>
+
+  </div></>;
+}
+
+export default App;
